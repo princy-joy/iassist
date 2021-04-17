f06eda1f18c8edef35c583ed8e4af3084cc85e09
princy-joy <princy.joy.au@gmail.com>
2021-04-16 20:43:03 -0700

    Flask webapp

---
+++ webapp/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,redirect,request
+from gtts import gTTS
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        input_image = request.files['input_image']
+        image_path = "./static/{}".format(input_image.filename) 
+        input_image.save(image_path)
+        audio_path = "./static/{}".format(input_image.filename) + ".mp3"
+
+        caption = 'model predict'
+        audio_out = gTTS(text = caption, lang = 'en',slow = False)
+        audio_out.save(audio_path)
+
+        predictions = {
+            'image' : image_path,
+            'caption' : caption,
+            'sound' : audio_path
+        }
+        return render_template("index.html", predictions = predictions)
+    return render_template("index.html")
+	
+
+if __name__ == '__main__':
+	app.run(debug = True)
